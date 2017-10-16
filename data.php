6e20f852872637df6c2b71bea9bc92882647ae94
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2017-10-16 12:50:54 +0200

    first array, probably not done

---
+++ data.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+declare(strict_types=1);
+
+//variabler och arrayer deklareras här
+
+
+$authorArray = [
+
+  "author1" => "Superman", 01,
+  "author2" => "Spiderman", 02,
+  "author3" => "Batman", 03,
+  "author4" => "Kalle Anka", 04,
+  "author5" => "Popeye", 05,
+
+];
+
+$contentArray = [
+
+
+
+];
+
+
+var_dump ($authorArray);
---
+++ phpassignment.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    /*
+      Authors och content behöver lagras i arrays, antingen i en multimensionell eller två separata.
+      The news feed items should contain:
+
+          - Title
+          - Content
+          - Author name
+          - Published date
+          - Like counter (the number of likes a news feed item has received)
+
+      The news feed author items should contain the following data:
+
+          - ID
+          - Full name
+
+
+    */
+
+
+
+  </body>
+</html>
