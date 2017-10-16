7c01b52c3fec54d6abda02bebf2152eb2e01317f
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2017-10-16 23:39:16 +0200

    added working array

--- data.php
+++ data.php
@@ -7,21 +7,65 @@ declare(strict_types=1);
 //variabler och arrayer deklareras här
 
 
-$authorArray = [
-
-  "author1" => "Superman", 01,
-  "author2" => "Spiderman", 02,
-  "author3" => "Batman", 03,
-  "author4" => "Kalle Anka", 04,
-  "author5" => "Popeye", 05,
-
-];
-
 $contentArray = [
 
+[
+    'name'     =>  'Spiderman',
+    'title1'   =>  'My very first post...',
+    'title2'   =>  'My very second post!',
+    'content1' =>  'Hello, web!',
+    'content2' =>  'When I grow up I want to be a web developer!',
+    'time'     =>  '2017-01-01',
+    'ID'       =>  001,
+  ],
 
+  [
+    'name'     =>  'Batman',
+    'title1'   =>  'My very first post...',
+    'title2'   =>  'My very second post!',
+    'content1' =>  'Hello, cave!',
+    'content2' =>  'Fucking scary, those damn bats!',
+    'time'     =>  '2017-01-02',
+    'ID'       =>  002,
+  ],
+
+  [
+    'name'     =>  'The Dude',
+    'title1'   =>  'My very first post...',
+    'title2'   =>  'My very second post!',
+    'content1' =>  'That\'s just like, your opinion, man.',
+    'content2' =>  'His wife goes out, owes money all over town, and they pee on MY rug!?!?',
+    'time'     =>  '2017-01-03',
+    'ID'       =>  003,
+  ],
+
+  [
+    'name'     =>  'Plato',
+    'title1'   =>  'My very first post...',
+    'title2'   =>  'My very second post!',
+    'content1' =>  'All I know is that I know nothing',
+    'content2' =>  'I like myself, alot!',
+    'time'     =>  '2017-01-04',
+    'ID'       =>  004,
+  ],
+
+  [
+    'name'     =>  'Aristotle',
+    'title1'   =>  'My very first post...',
+    'title2'   =>  'My very second post!',
+    'content1' =>  'Something, something, Eudaimonia.',
+    'content2' =>  'The human being is a social creature, fo sho!',
+    'time'     =>  '2017-01-05',
+    'ID'       =>  005,
+  ],
 
 ];
 
 
-var_dump ($authorArray);
+
+    var_dump($contentArray);
+
+
+
+
+    // echo $contentArray[2]['content2'];
