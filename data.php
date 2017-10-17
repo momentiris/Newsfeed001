23f6465173e33ac3dbdff2384d52a53a8d3127d0
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2017-10-17 10:06:56 +0200

    Testrunda 1 loops

--- data.php
+++ data.php
@@ -12,67 +12,117 @@ $contentArrays = [
 [
     'name'     =>  'Spiderman',
     'title1'   =>  'My very first post...',
-    'title2'   =>  'My very second post!',
+
     'content1' =>  'Hello, web!',
-    'content2' =>  'When I grow up I want to be a web developer!',
+
     'time'     =>  '2017-01-01',
     'ID'       =>  001,
   ],
 
   [
+      'name'     =>  'Spiderman',
+
+      'title2'   =>  'My very second post!',
+
+      'content2' =>  'When I grow up I want to be a web developer!',
+      'time'     =>  '2017-01-03',
+      'ID'       =>  001,
+    ],
+  [
     'name'     =>  'Batman',
     'title1'   =>  'My very first post...',
-    'title2'   =>  'My very second post!',
+
     'content1' =>  'Hello, cave!',
-    'content2' =>  'Fucking scary, those damn bats!',
+
     'time'     =>  '2017-01-02',
     'ID'       =>  002,
   ],
+  [
+    'name'     =>  'Batman',
+
+    'title2'   =>  'My very second post!',
+
+    'content2' =>  'Fucking scary, those damn bats!',
+    'time'     =>  '2017-01-04',
+    'ID'       =>  002,
+  ],
+
 
   [
     'name'     =>  'The Dude',
     'title1'   =>  'My very first post...',
-    'title2'   =>  'My very second post!',
+
     'content1' =>  'That\'s just like, your opinion, man.',
+
+    'time'     =>  '2017-01-05',
+    'ID'       =>  003,
+  ],
+
+  [
+    'name'     =>  'The Dude',
+
+    'title2'   =>  'My very second post!',
+
     'content2' =>  'His wife goes out, owes money all over town, and they pee on MY rug!?!?',
-    'time'     =>  '2017-01-03',
+    'time'     =>  '2017-01-07',
     'ID'       =>  003,
   ],
 
   [
     'name'     =>  'Plato',
     'title1'   =>  'My very first post...',
-    'title2'   =>  'My very second post!',
+
     'content1' =>  'All I know is that I know nothing',
-    'content2' =>  'I like myself, alot!',
-    'time'     =>  '2017-01-04',
+
+    'time'     =>  '2017-01-11',
     'ID'       =>  004,
   ],
 
   [
+    'name'     =>  'Plato',
+
+    'title2'   =>  'My very second post!',
+
+    'content2' =>  'I like myself, alot!',
+    'time'     =>  '2017-02-04',
+    'ID'       =>  004,
+  ],
+
+
+  [
     'name'     =>  'Aristotle',
     'title1'   =>  'My very first post...',
-    'title2'   =>  'My very second post!',
+
     'content1' =>  'Something, something, Eudaimonia.',
-    'content2' =>  'The human being is a social creature, fo sho!',
-    'time'     =>  '2017-01-05',
+
+    'time'     =>  '2017-03-05',
     'ID'       =>  005,
+  ],
+
+  [
+    'name'     =>  'Aristotle',
+
+    'title2'   =>  'My very second post!',
+
+    'content2' =>  'The human being is a social creature, fo sho!',
+    'time'     =>  '2017-01-06',
+    'ID'       =>  5,
   ],
 
 ];
 
 //Ska här loopa fram min lagrade information, för tillfället lost.
 
-foreach ($contentArrays as $contentArray) {
 
-  foreach ($contentArray as $key => $value) {
 
-      if ($key >= '0') {
-
+foreach ($contentArrays as $posts) {
+    foreach ($posts as $post => $value) {
+      if (!strcmp($post, 'ID')&&!strcmp((string)$value, '5')) {
+          print_r($posts);
       }
-  }
-
+    }
 }
+
 
 
 
